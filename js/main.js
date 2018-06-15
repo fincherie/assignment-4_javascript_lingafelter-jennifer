a83ba32d8b08192abd2083866c482652d9c16e2d
Jennifer Lingafelter <jlingafelter@gmail.com>
2018-06-15 10:57:20 -0400

    adding functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-var myName = {
+//*var myName = {
     firstName: "Jennifer",
     lastName: "Lingafelter",
     howdy: function () {
       return this.firstName + " " + this.lastName +" says Hello!";
 }
 }
+
 myName.howdy();
+
+var numba = prompt ("Enter your string");
+if (numba.length<7) {
+window.alert ("What a short little word");
+}
+else if (numba.length >7) {
+  window.alert("I'm sorry, but that's too many to count.");
+}
+else {
+  window.alert("That perfect!");
+}
+
+
+function inception (display, favMovie) {
+ display (favMovie);
+ favMovie = "Dead Pool"
+}
+ console.log (favMovie + "is a fantastic movie.");
