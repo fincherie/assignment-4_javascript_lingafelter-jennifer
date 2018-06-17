ce95e0c64f727c30a08e57488c12edba5300346d
Jennifer Lingafelter <jlingafelter@gmail.com>
2018-06-17 07:00:13 -0400

    fixed the lost presidents array

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var pres45 = {firstName: "Barack", lastName: "Obama", termLength: 8, party: "Democrat", yearsOfPresidency: "2009-2017"};
+var pres44 = {firstName: "George W.", lastName: "Bush", termLength: 8, party: "Republican", yearsOfPresidency: "2001-2009"};
+var pres43 = {firstName: "William", lastName: "Clinton", termLength: 8, party: "Democrat", yearsOfPresidency: "1993-2001"};
+var pres42 = {firstName: "George", lastName: "Bush", termLength: 4, party: "Republican", yearsOfPresidency: "1989-1993"};
+var pres41 = {firstName: "Ronald", lastName: "Reagan", termLength: 8, party: "Republican", yearsOfPresidency: "1981=1989"};
+
+var presidents = [pres45, pres44, pres43, pres42, pres41];
+console.log (presidents[1].firstName + presidents[1].lastName);
